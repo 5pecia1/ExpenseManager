ee53bafd3eed035ccf630aa27bca3543b58c5cec
5pecia1 <anmenmolsu22@naver.com>
2016-11-11 16:06:12 +0900

    Add format, prevent exception

--- app/src/main/java/sol_5pecia1/expense_manager/view/MoneyFormatView.java
+++ app/src/main/java/sol_5pecia1/expense_manager/view/MoneyFormatView.java
@@ -5,15 +5,16 @@ import android.content.res.TypedArray;
 import android.util.AttributeSet;
 import android.widget.TextView;
 
-import lombok.Data;
+import lombok.Getter;
 import sol_5pecia1.expense_manager.R;
 
 /**
  * Created by 5pecia1 on 2016-11-11.
  */
-@Data
 public class MoneyFormatView extends TextView{
-    private String money = "";
+    @Getter
+    private String money;
+    private String format;
 
     public MoneyFormatView(Context context) {
         this(context, null);
@@ -22,11 +23,32 @@ public class MoneyFormatView extends TextView{
     public MoneyFormatView(Context context, AttributeSet attributeSet) {
         super(context, attributeSet);
 
-
         TypedArray typedArray = context.obtainStyledAttributes(attributeSet, R.styleable.MoneyFormatView);
-        String typedMoney = typedArray.getString(R.styleable.MoneyFormatView_money);
-        money = (typedMoney == null)? "0": typedMoney;
 
-        setText(context.getString(R.string.money_format, money));
+        String typedMoney = typedArray.getString(R.styleable.MoneyFormatView_money);
+        String inputFormat = typedArray.getString(R.styleable.MoneyFormatView_format);
+
+        money = (typedMoney == null)? "0" : typedMoney;
+        format = (inputFormat == null)
+                ? context.getString(R.string.default_money_format)
+                : inputFormat;
+
+
+        setMoney(money);
+    }
+
+    public void setMoney(String money) {
+        setMoney(money, format);
+    }
+
+    public void setMoney(String money, String format) {
+        this.money = money;
+        this.format = format;
+
+        if (format.split("%s").length != 2) { // prevent exception
+            format = String.format(format, money, "%s");
+        }
+
+        setText(String.format(format, money));
     }
 }
